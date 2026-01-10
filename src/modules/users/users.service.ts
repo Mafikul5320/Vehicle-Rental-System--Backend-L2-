1948772bec7ea621f5e4fc1cc197c897f39ef3c1
Mafikul Islam <mdmafikulislam5320@gmail.com>
2026-01-10 23:19:57 +0600

    auth route created

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,12 @@
 import { pool } from "../../config/DB";
+import bcrypt from "bcryptjs";
 
 const userCreate = async (payload: Record<string, unknown>) => {
     const { name, email, password, phone, role } = payload;
-    const result = await pool.query(`INSERT INTO users(name,email,password,phone,role) VALUES($1, $2, $3, $4, $5) RETURNING *`, [name, email, password, phone, role]);
+
+    const hashPassword = bcrypt.hashSync(password as string, 10);
+
+    const result = await pool.query(`INSERT INTO users(name,email,password,phone,role) VALUES($1, $2, $3, $4, $5) RETURNING *`, [name, email, hashPassword, phone, role]);
     return result;
 };
 
